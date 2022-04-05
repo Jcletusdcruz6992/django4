2676fc2a55c4f15c5ee01eaf27bf1e83a967d4c1
James Cletus Dcruz <jamescletusdcruz@gmail.com>
2022-04-05 11:41:37 -0400

    2022-04-05-11-41am

--- ProFour/AppFour/admin.py
+++ ProFour/AppFour/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from AppFour.models import Topic,Webpage,AccessRecord
 # Register your models here.
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
+admin.site.register(Topic)
